9b219b8b3945fd326682304c6fe04d3becf83909
arnavboppudi <105809704+arnavboppudi@users.noreply.github.com>
2023-08-10 17:10:35 -0400

    Add files via upload

---
+++ FINAL PROJECT_OFFICIAL/setup.py
@@ -0,0 +1,10 @@
+
+from setuptools import setup
+
+setup(name = 'Final Project',
+    author = 'Arnav Boppuid',
+    author_email = 'ab2kk@virginia.edu',
+    packages = ['montecarlopack','unittesting'],
+    url = 'https://github.com/arnavboppudi/ab2kk_ds5100_montecarlo?search=1',
+    license = 'none',
+    description = 'DS 5100 Final Project',
